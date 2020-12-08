022e92aea94fce769ab2da8ae8bb608e70d9138f
Your name <951148962@qq.com>
2020-12-08 15:38:49 +0800

    lodash

---
+++ lodash/gaoguoyiwrite-lodash.js
@@ -0,0 +1,16 @@
+var gaoguoyiwrite = {
+  compact: function (ary) {
+    var resault = []
+    for (var i = 0; i < ary.length; i++) {
+      if (ary[i]) {
+        resault.push(ary[i])
+      }
+    }
+    return resault
+  }
+
+
+
+
+
+}
--- lodash/gaoxing_guo-lodash.js
+++
@@ -1,3 +0,0 @@
-var gaoguo_yi = {
-
-}
